337d7dec664dfa3f471b57d268c7dc292e863ee6
hallasillusco <grovergonzi@gmail.com>
2025-03-03 19:14:23 -0400

    agregado2 en el main

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,14 +3,27 @@
 class Conexion{
   
   static public function conectar(){
-    $host="ballast.proxy.rlwy.net";
-    $db="railway";
+    $host="localhost";
+    $db="pos";
     $userDB="root";
-    $passDB="bXghUUpWLcstSJapTdNBYqgozDipggaI";
-    mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
+    $passDB="";
+    mysql://root:qEpAkkRgztGhrXyvVHPmXPElubzhDmNE@switchback.proxy.rlwy.net:54258/railway
+    ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
     $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
     $link->exec("set names utf8");
     return $link;
   } 
+  // class Conexion{
+  
+  //   static public function conectar(){
+  //     $host="localhost";
+  //     $db="pos";
+  //     $userDB="root";
+  //     $passDB="";
+  //     ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
+  //     $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
+  //     $link->exec("set names utf8");
+  //     return $link;
+  //   } 
   
 }
