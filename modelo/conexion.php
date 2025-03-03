70eeb359c1e8240777798d894e5a8895feac9679
hallasillusco <grovergonzi@gmail.com>
2025-03-02 22:12:32 -0400

    se agrega

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,11 +3,11 @@
 class Conexion{
   
   static public function conectar(){
-    $host="localhost";
-    $db="pos";
+    $host="ballast.proxy.rlwy.net";
+    $db="railway";
     $userDB="root";
-    $passDB="";
-    
+    $passDB="bXghUUpWLcstSJapTdNBYqgozDipggaI";
+    mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
     $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
     $link->exec("set names utf8");
     return $link;
