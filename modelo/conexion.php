8a29ad54d4de75e24fe296cf972834938ba98b10
hallasillusco <grovergonzi@gmail.com>
2025-03-03 22:47:21 -0400

    mod

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,30 +1,16 @@
 <?php
 
-// class Conexion{
-  
-//   static public function conectar(){
-//     $host="switchback.proxy.rlwy.net";
-//     $db="pos";
-//     $userDB="root";
-//     $passDB="qEpAkkRgztGhrXyvVHPmXPElubzhDmNE";
-//     //$portDB="8080";
-//     mysql://root:qEpAkkRgztGhrXyvVHPmXPElubzhDmNE@switchback.proxy.rlwy.net:54258/railway
-//     ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
-//     $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
-//     $link->exec("set names utf8");
-//     return $link;
-//   } 
+
   class Conexion{
   
    static public function conectar(){
-       $host="yamabiko.proxy.rlwy.net";
-     $db="railway";
+       $host="localhost";
+     $db="pos";
        $userDB="root";
-   $passDB="GIqLftZnKUMpBaBXRYwVThdZGlCQaNMH";
+   $passDB="";
          // mysql://root:GIqLftZnKUMpBaBXRYwVThdZGlCQaNMH@yamabiko.proxy.rlwy.net:22759/railway
-       ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
-       $link=new PDO("mysql:host=".$host."yamabiko.proxy.rlwy.net"."dbname=".$db, $userDB, $passDB);
-       //$link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
+      
+       $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
        $link->exec("set names utf8");
        return $link;
      } 
