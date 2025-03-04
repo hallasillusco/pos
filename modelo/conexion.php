da6a2c4e2d9ff69faf0e4d1807435caa2a407d8b
hallasillusco <grovergonzi@gmail.com>
2025-03-03 22:39:17 -0400

    nuevo

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -23,7 +23,8 @@
    $passDB="GIqLftZnKUMpBaBXRYwVThdZGlCQaNMH";
          // mysql://root:GIqLftZnKUMpBaBXRYwVThdZGlCQaNMH@yamabiko.proxy.rlwy.net:22759/railway
        ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
-       $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
+       $link=new PDO("mysql:host=".$host."yamabiko.proxy.rlwy.net"."dbname=".$db, $userDB, $passDB);
+       //$link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
        $link->exec("set names utf8");
        return $link;
      } 
