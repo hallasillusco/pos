693248bf74c855aa0fdd4cdef90de6e9afb31fbb
hallasillusco <grovergonzi@gmail.com>
2025-03-03 22:14:34 -0400

    cambio

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -17,10 +17,11 @@
   class Conexion{
   
    static public function conectar(){
-       $host="localhost";
+       $host="yamabiko.proxy.rlwy.net";
      $db="pos";
        $userDB="root";
-   $passDB="";
+   $passDB="GIqLftZnKUMpBaBXRYwVThdZGlCQaNMH";
+         // mysql://root:GIqLftZnKUMpBaBXRYwVThdZGlCQaNMH@yamabiko.proxy.rlwy.net:22759/railway
        ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
        $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
        $link->exec("set names utf8");
