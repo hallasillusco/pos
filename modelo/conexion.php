0cab1caa9fe9a3166b5f3869e11f6c84a00c065c
hallasillusco <grovergonzi@gmail.com>
2025-03-03 20:24:32 -0400

    otrocambio

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,30 +1,30 @@
 <?php
 
-class Conexion{
+// class Conexion{
   
-  static public function conectar(){
-    $host="switchback.proxy.rlwy.net";
-    $db="pos";
-    $userDB="root";
-    $passDB="qEpAkkRgztGhrXyvVHPmXPElubzhDmNE";
-    //$portDB="8080";
-    mysql://root:qEpAkkRgztGhrXyvVHPmXPElubzhDmNE@switchback.proxy.rlwy.net:54258/railway
-    ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
-    $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
-    $link->exec("set names utf8");
-    return $link;
-  } 
-  // class Conexion{
+//   static public function conectar(){
+//     $host="switchback.proxy.rlwy.net";
+//     $db="pos";
+//     $userDB="root";
+//     $passDB="qEpAkkRgztGhrXyvVHPmXPElubzhDmNE";
+//     //$portDB="8080";
+//     mysql://root:qEpAkkRgztGhrXyvVHPmXPElubzhDmNE@switchback.proxy.rlwy.net:54258/railway
+//     ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
+//     $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
+//     $link->exec("set names utf8");
+//     return $link;
+//   } 
+  class Conexion{
   
-  //   static public function conectar(){
-  //     $host="localhost";
-  //     $db="pos";
-  //     $userDB="root";
-  //     $passDB="";
-  //     ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
-  //     $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
-  //     $link->exec("set names utf8");
-  //     return $link;
-  //   } 
+   static public function conectar(){
+       $host="localhost";
+     $db="pos";
+       $userDB="root";
+   $passDB="";
+       ///mysql://root:bXghUUpWLcstSJapTdNBYqgozDipggaI@ballast.proxy.rlwy.net:26325/railway
+       $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
+       $link->exec("set names utf8");
+       return $link;
+     } 
   
 }
